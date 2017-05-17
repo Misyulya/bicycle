536a17c772ee7900b2b6ecef9fd2ceb84199e711
D.Misiulia <d.misiulia@hackspace.by>
2017-05-17 09:58:24 +0300

    [ADD] Android Calligraphy library

--- app/src/main/java/com/example/dmitry/bicycle/adapter/SampleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/dmitry/bicycle/adapter/SampleFragmentPagerAdapter.java
@@ -17,22 +17,12 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
 
     final int PAGE_COUNT = 3;
     private String mTabTitles[] = new String[]{"Map", "Rentals", "Profile"};
-    private int[] mImageResId = {R.drawable.ic_add_location_black_24dp,
-            R.drawable.ic_directions_bike_black_24dp, R.drawable.ic_help_outline_black_24dp};
     private Context context;
     private String mIcons[];
-
-    private String mMapIcon;
-    private String mBicycleIcon;
-    private String mUserIcon;
-
 
     public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
-        mMapIcon = context.getResources().getString(R.string.fa_icon_globe);
-        mBicycleIcon = context.getResources().getString(R.string.fa_icon_bicycle);
-        mUserIcon = context.getResources().getString(R.string.fa_icon_user);
         mIcons = context.getResources().getStringArray(R.array.fa_tabs_icons);
     }
 
@@ -51,15 +41,14 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
         // Generate title based on item position
         return mTabTitles[position];
     }
+
     public View getTabView(int position) {
-        // Given you have a custom layout in `res/layout/custom_tab.xml` with a TextView and ImageView
+        // Given you have a custom layout in `res/layout/custom_tab.xml`
         View v = LayoutInflater.from(context).inflate(R.layout.tab_view, null);
         TextView tv = (TextView) v.findViewById(R.id.tab_text_view);
         tv.setText(mTabTitles[position]);
         TextView iconTextView = (TextView) v.findViewById(R.id.tab_icon_text_view);
         iconTextView.setText(mIcons[position]);
-//        ImageView img = (ImageView) v.findViewById(R.id.tab_image_view);
-//        img.setImageResource(mImageResId[position]);
         return v;
     }
 }
